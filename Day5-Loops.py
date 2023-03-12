2d8b92c86bf5774900cf3523de18aab425ab44b3
CypherDragon <cm.cypherdragon@gmail.com>
2023-03-12 11:46:57 -0500

    Initial commit

---
+++ Day5-Loops.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    i = 1
+
+while i < 11:
+    out = n * i
+    print(str(n) + " x " + str(i) + " = " + str(out))
+    i += 1
+
